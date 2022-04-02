e87ba3306237ad064bd79885376b546fdb560dd0
Alírio de Souza Rios <alirioriosmc@gmail.com>
2022-04-02 14:51:02 -0300

    update js

--- stopwatch-js/js/script.js
+++ stopwatch-js/js/script.js
@@ -3,9 +3,38 @@ const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
 
+const nextYear = new Date().getFullYear() + 1;
+const newYear = new Date(`January 01 ${nextYear} 00:00:00`);
+
+let interval = setInterval(() => CountDown(), second);
+
+window.document.getElementById('nextYear').innerText = nextYear;
+
+function CountDown() {
+    let currentTime = new Date();
+    let difference = newYear - currentTime;
+
+    let currentDay = Math.floor(difference / day);
+    let currentHour = Math.floor((difference % day) / hour);
+    let currentMinute = Math.floor((difference % hour) / minute);
+    let currentSecond = Math.floor((difference % minute) / second);
+
+    window.document.getElementById('count_day').innerText = currentDay < 10 ? '0' + currentDay : currentDay;
+    window.document.getElementById('count_hour').innerText = currentHour < 10 ? '0' + currentHour : currentHour;
+    window.document.getElementById('count_min').innerText = currentMinute < 10 ? '0' + currentMinute : currentMinute;
+    window.document.getElementById('count_sec').innerText = currentSecond < 10 ? '0' + currentSecond : currentSecond;
+} 
+
+
+/* BACKUP ESTÁTICO ------------------------------------------------------------------------------------------------ */
+/* 
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 const nextYear = new Date('01/01/2023 00:00:00').getTime();
-const newYear = new Date(`January 01 ${nextYear} 00:00:00`); /* Alocar o proximo ano dinâmicamente (VERIFICAR) */
+const newYear = new Date(`January 01 ${nextYear} 00:00:00`);
 
 let interval = setInterval(() => CountDown(), second);
 
@@ -22,11 +51,5 @@ function CountDown() {
     window.document.getElementById('count_hour').innerText = currentHour < 10 ? '0' + currentHour : currentHour;
     window.document.getElementById('count_min').innerText = currentMinute < 10 ? '0' + currentMinute : currentMinute;
     window.document.getElementById('count_sec').innerText = currentSecond < 10 ? '0' + currentSecond : currentSecond;
-}
-
-/* AJUSTANDO O ANO DINÂMICAMENTE (SOLUCIONAR)
-
-const nextYear = new Date().getFullYear() + 1;
-const newYear = new Date(`January 01 ${nextYear} 00:00:00`);
-
+} 
 */
