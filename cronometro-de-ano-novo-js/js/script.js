456a5d239889c5cc3398d303d23d404029d9c217
Alírio de Souza Rios <alirioriosmc@gmail.com>
2022-04-06 14:18:13 -0300

    add comment

--- cronometro-de-ano-novo-js/js/script.js
+++ cronometro-de-ano-novo-js/js/script.js
@@ -1,24 +1,51 @@
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
+/*
+Variáveis de tempo:
+- Vamos utilizar dos métodos de Date.
+- Date retorna valores de tempo em ms (milissegundos)
+- Necessário realizar o cálculo de conversão de ms para cada variável
+*/
+const second = 1000; /* 1 s == 1000 ms */
+const minute = second * 60; /* 1 minuto tem 60 segundos */
+const hour = minute * 60; /* 1 hora tem 60 minutos */
+const day = hour * 24; /* 1 dia tem 24 horas */
 
-const nextYear = new Date().getFullYear() + 1;
-const newYear = new Date(`January 01 ${nextYear} 00:00:00`);
+const nextYear = new Date().getFullYear() + 1; /* Recebendo o ano atual do sistema (+ 1 para ter o próximo; utilizado no script) */
+const newYear = new Date(`January 01 ${nextYear} 00:00:00`); /* Ajustando dinâmicamente o próximo ano do cálculo */
 
+/* 
+setInterval:
+- Precisa ser instânciado (declarado).
+- Faz chamadas de funções e métodos repetidamente com ajuste de intervalo.
+- setInterval (<função>, <tempo>(second == 1000)).
+- Na própria chamada é utilizado também o "array function".
+*/
+
+/*
+Array Function:
+- 
+*/
 let interval = setInterval(() => CountDown(), second);
 
-window.document.getElementById('nextYear').innerText = nextYear;
+window.document.getElementById('nextYear').innerText = nextYear; /* Apenas exibindo o próximo ano */
 
 function CountDown() {
-    let currentTime = new Date();
-    let difference = newYear - currentTime;
+    let currentTime = new Date(); /* Tempo atual (millisegundos) */
+    let difference = newYear - currentTime; /* Pegando em ms o tempo restante até o PRÓXIMO ANO */
 
+    /* 
+    Cálculos para as variáveis:
+    - Lembrando que as variáveis em questão estão guardando valores em ms (milissegundos).
+    - 
+    */
     let currentDay = Math.floor(difference / day);
     let currentHour = Math.floor((difference % day) / hour);
     let currentMinute = Math.floor((difference % hour) / minute);
     let currentSecond = Math.floor((difference % minute) / second);
 
+    /* 
+    Exibindo o resultado:
+    - Utilizando CONDICIONAL TERNÁRIO para melhorar a visualização.
+    */
     window.document.getElementById('count_day').innerText = currentDay < 10 ? '0' + currentDay : currentDay;
     window.document.getElementById('count_hour').innerText = currentHour < 10 ? '0' + currentHour : currentHour;
     window.document.getElementById('count_min').innerText = currentMinute < 10 ? '0' + currentMinute : currentMinute;
